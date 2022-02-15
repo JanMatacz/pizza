869e1b827fa78fcfe89bb6b9326fcd963677b46c
JanMatacz <zcatam@gmail.com>
2022-02-15 09:09:42 +0100

    PA

---
+++ src/main/java/pl/matacz/pizzaaplication/remote/rest/dto/response/OrderCollectionDto.java
@@ -0,0 +1,30 @@
+package pl.matacz.pizzaaplication.remote.rest.dto.response;
+
+import pl.matacz.pizzaaplication.remote.rest.dto.request.PersonOrderDto;
+
+import java.util.List;
+
+public class OrderCollectionDto
+{
+    private List<OrderDto> orders;
+
+    public OrderCollectionDto()
+    {
+
+    }
+
+    public OrderCollectionDto(List<OrderDto> orders)
+    {
+        this.orders = orders;
+    }
+
+    public List<OrderDto> getOrders()
+    {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDto> orders)
+    {
+        this.orders = orders;
+    }
+}
---
+++ src/test/java/pl/matacz/pizzaaplication/PizzaAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matacz.pizzaaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
